f327feef153d782667c25ee695d208b3d0af0a5d
Jonathan Hudak <hudak.jonathan@gmail.com>
2018-12-24 09:10:08 -0800

    Updates

---
+++ precache-manifest.600fe0c233c70b610f6f7ae06fc8132f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bebd1ad00be3adadb05",
+    "url": "/todo-today/static/js/runtime~main.5bebd1ad.js"
+  },
+  {
+    "revision": "7afcf38ec259dbca2b8d",
+    "url": "/todo-today/static/js/main.7afcf38e.chunk.js"
+  },
+  {
+    "revision": "55aea75aeff2c6a7b174",
+    "url": "/todo-today/static/js/1.55aea75a.chunk.js"
+  },
+  {
+    "revision": "7afcf38ec259dbca2b8d",
+    "url": "/todo-today/static/css/main.666f32ae.chunk.css"
+  },
+  {
+    "revision": "e7153b1fc896c889f1ff22a36e6242e0",
+    "url": "/todo-today/index.html"
+  }
+];
